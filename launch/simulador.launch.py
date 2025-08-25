48b01fa8c235a214abc2c2bed354919327fb48be
DronKab <dronkab.unaq@gmail.com>
2025-08-25 15:14:02 -0600

    First commit

---
+++ launch/simulador.launch.py
@@ -0,0 +1,25 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
+
+def generate_launch_description():
+    return LaunchDescription([
+        ExecuteProcess(
+            cmd=["MicroXRCEAgent", "udp4","--port","8888"],
+            output="log"
+        ),
+        Node(
+            package="tmr25",
+            executable="px4_driver",
+            output="screen"
+        ),
+        Node(
+            package="aruco_opencv",
+            executable="aruco_tracker_autostart",
+            output="screen",
+            parameters=[{
+                "cam_base_topic":"/pi_camera/image_raw",
+                "marker_dict":"5X5_1000"
+            }]
+        )
+    ])
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+import os 
+from glob import glob
+
+package_name = 'imav25'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*.launch.py'))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='dronkab',
+    maintainer_email='dronkab.unaq@gmail.com',
+    description='TODO: Package description',
+    license='MIT',
+    entry_points={
+        'console_scripts': [
+            "px4_driver = imav25.px4_driver:main"
+        ],
+    },
+)
